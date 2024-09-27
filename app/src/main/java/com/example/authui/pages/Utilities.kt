0d2c2e27c3df44e133256b7b0ded7e1ab81fae8a
Maleesha Fonseka <maleeshafonseka2020@gmail.com>
2024-09-27 12:07:53 +0530

    UI updated

--- app/src/main/java/com/example/authui/pages/Utilities.kt
+++ app/src/main/java/com/example/authui/pages/Utilities.kt
@@ -1,17 +1,15 @@
 package com.example.authui.pages
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
@@ -20,14 +18,11 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,18 +30,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.Visibility
 import com.example.authui.R
 
 @Composable
@@ -193,6 +184,28 @@ fun ScreenTabs() {
 }
 
 @Composable
+fun CheckboxWithText () {
+    var checked by remember { mutableStateOf(true) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = it },
+            Modifier.size(32 .dp)
+        )
+        Text(
+            "Remember me",
+            color = colorResource(R.color.darkTeal),
+            fontWeight = FontWeight(700),
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.End,
+        )
+    }
+}
+
+@Composable
 fun LoginForm() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
@@ -250,21 +263,26 @@ fun LoginForm() {
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            ClickableText(
-                text = AnnotatedString("Forget Password ?"),
-                onClick = {
-                    // Handle forget password click
-                    // navigate to a forget password screen
-                },
-                style = LocalTextStyle.current.copy(
-                    color = colorResource(R.color.darkTeal),
-                    fontWeight = FontWeight(700),
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.End,
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                CheckboxWithText()
+                ClickableText(
+                    text = AnnotatedString("Forget Password ?"),
+                    onClick = {
+                        // Handle forget password click
+                        // navigate to a forget password screen
+                    },
+                    style = LocalTextStyle.current.copy(
+                        color = colorResource(R.color.darkTeal),
+                        fontWeight = FontWeight(700),
+                        fontStyle = FontStyle.Italic,
+                        textAlign = TextAlign.End,
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
